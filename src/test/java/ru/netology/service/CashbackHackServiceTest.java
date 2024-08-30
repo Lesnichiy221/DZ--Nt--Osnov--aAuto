b69da052574039580a7bd7859c55f91c1b52212f
Vlad Test <vladkretovsky@yandex.ru>
2024-08-30 15:26:44 +0300

    Added Junit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,41 +1,42 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class CashbackHackServiceTest {
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
 
     @Test
-    void whatIf1000(){
+    public void whatIf1000(){
 
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1000);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void whatIfMoreThen1000(){
+    public void whatIfMoreThen1000(){
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1100);
         int expected = 900;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void whatIfBelowThen1000(){
+    public void whatIfBelowThen1000(){
 
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(900);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
